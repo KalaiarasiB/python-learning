079991d70ff50514ad2e2d47d615acb281c3a5a7
KalaiarasiB <iamkalaiarasi23@gmail.com>
2025-12-29 19:27:30 +0530

    Day 1: Python print basics

---
+++ day1_print_basics.py
@@ -0,0 +1,28 @@
+# Day 1: Python Print Basics
+
+# 1. Simple print
+print("Hello, Python")
+
+# 2. Printing multiple values
+print("AI", "Engineering", "Journey")
+
+# 3. sep argument
+print("AI", "Engineering", "Journey", sep=" - ")
+print("2025", "12", "18", sep="/")
+
+# 4. end argument
+print("Learning Python", end=" ")
+print("Day 1")
+
+print("Loading", end="...")
+print("Done")
+
+# 5. Escape sequences
+print("Hello\nWorld")      # New line
+print("Hello\tWorld")      # Tab space
+print("She said \"Python is easy\"")
+print("This is a backslash \\")
+
+# 6. Mixing everything
+print("Name", "Kalai", sep=": ", end=" | ")
+print("Goal", "AI Engineer", sep=": ")
