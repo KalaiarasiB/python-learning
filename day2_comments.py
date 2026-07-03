3dcbcdfff9debe70e1d966df4781b5b8a937a9fc
KalaiarasiB <iamkalaiarasi23@gmail.com>
2026-07-03 19:53:40 +0530

    Add Day 2 Python comments

---
+++ day2_comments.py
@@ -0,0 +1,10 @@
+#print("this is a single line comment")
+
+''' this is a multi-line comment
+print("this is a print statement")
+print("ml is fun")
+'''
+""" this is a multi line comment
+print("this is a print statement")
+print("ml is fun")
+"""
