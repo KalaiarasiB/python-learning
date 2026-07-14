563b92af621820fb10d7e8987de708cec12cefbd
KalaiarasiB <iamkalaiarasi23@gmail.com>
2026-07-14 21:50:19 +0530

    Add day 3 variables&casting

---
+++ day3_variables&casting.py
@@ -0,0 +1,36 @@
+ # using type()
+name="kalai"
+int(type(name))
+#using id()
+print(id(name))
+#declaring value
+a,b=1,2
+print(a,b)
+#swaping
+a,b=b,a
+print(a)
+print(b)
+#casting
+a = "kalai"
+a=bool(a)
+print(a)
+f=2+3j
+f=bool(f)
+print(f)
+
+#logical operators
+mail = True
+mess = True
+print(mail&mess)
+#membership operator
+fruit = "apple"
+print("z" not in "apple")
+print("a" in "apple")
+#assignment operator
+a+=10
+print(a)
+a-=20
+print(a)
+a=2
+a*=10
+print(a)
