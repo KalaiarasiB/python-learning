c05eea5be6468a3f695fd505f16dc0decce2068a
KalaiarasiB <iamkalaiarasi23@gmail.com>
2026-07-14 21:58:13 +0530

    Add day 4 if_else

---
+++ day 4_if _else.py
@@ -0,0 +1,66 @@
+#if_else
+age = 16
+if age >=18:
+    print("eligible")
+else:
+    print("not eligible")
+#if_elif_else
+marks = 40
+if marks >=90:
+  print("o")
+elif marks > 70:
+   print("a")
+else:
+   print("just pass")
+   #whether the given number is two digit number or not
+   num = 20
+   if num >= 10 & num < 100:
+    print("two digit number")
+   else:
+      print("not a two digit")
+# whether the number is positive or negative
+num = -30
+if num < 0 & num > 0:
+   print("it is a positive number")
+else:
+   print("it is a negative")
+   # whether the number is greater or not
+   a = int(input("enter the number:"))
+   b = int(input("enter the number:"))
+   if a > b :
+      print("it is greater")
+   else:
+      print("it is smaller")
+#check whether a number is positive,negative or zero
+n = 0
+if n>0:
+   print("it is positive")
+elif n<0:
+    print("it is negative")
+else:
+   print("it is zero")
+#check wbhether the number is 3 digit or not
+num = 9900
+if num>=100 & num<1000:
+   print("three digit")
+else:
+   print("not three digit")
+#whether the number is divisible by 2,3 or more
+num = int(input("enter the value"))
+if num % 2 ==0 & num % 3 ==0:
+   print("both")
+elif num % 2==0:
+   print("divisible by 2")
+elif  num % 3==0:
+   print("divisibloe by 3")
+else:
+   print("neither")
+# check whether the number is divisible by 7 but not 5
+num = int (input("enter the number"))
+if num % 7==0 & num !=5:
+   print("divisible by 7 but not 5")
+else:
+   print("not divisible")
+
+   
+   
